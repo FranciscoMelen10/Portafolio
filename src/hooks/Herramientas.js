b12160c9b5906f713082bfbdf716aa72bb8a16a1
Francisco_Melendez <fmelendezsimplina@gmail.com>
2024-09-15 14:10:31 -0600

    Feat: Arreglando la carga de datos de los proyectos
    Gracias pocketbase por:

--- src/hooks/Herramientas.js
+++
@@ -1,7 +0,0 @@
-import PocketBase from "pocketbase";
-
-const pb = new PocketBase("https://portafolio.pockethost.io");
-
-const herramientas = await pb.collection("Herramientas").getFullList({});
-
-export { herramientas }
--- src/hooks/Proyectos.js
+++
@@ -1,8 +0,0 @@
-import PocketBase from "pocketbase";
-
-const pb = new PocketBase("https://portafolio.pockethost.io");
-
-const proyectos = await pb.collection("Proyectos").getFullList({expand: ["herramientasId"]});
-
-export { proyectos }
-
